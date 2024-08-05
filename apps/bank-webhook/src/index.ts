2578ac074ee90b58f7368d2f68ff061dd7990909
Vaibhav <vaibhavmhaske.here@gmail.com>
2024-08-06 00:50:41 +0530

    some changes

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,37 +1,39 @@
-import db from "@repo/db/client";
 import express from "express";
-
+import db from "@repo/db/client";
 const app = express();
-const port = 3000;
 
+app.use(express.json())
 
 app.post("/hdfcWebhook", async (req, res) => {
-    //TODO: Add zod validations here?
+    //TODO: Add zod validation here?
+    //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
     const paymentInformation: {
         token: string;
         userId: string;
-        amount: string;
+        amount: string
     } = {
         token: req.body.token,
         userId: req.body.user_identifier,
         amount: req.body.amount
-    }
+    };
+
     try {
         await db.$transaction([
-            db.balance.update({
+            db.balance.updateMany({
                 where: {
                     userId: Number(paymentInformation.userId)
                 },
                 data: {
                     amount: {
+                        // You can also get this from your DB
                         increment: Number(paymentInformation.amount)
                     }
                 }
             }),
-            db.onRampTransaction.update({
+            db.onRampTransaction.updateMany({
                 where: {
                     token: paymentInformation.token
-                },
+                }, 
                 data: {
                     status: "Success",
                 }
@@ -41,14 +43,13 @@ app.post("/hdfcWebhook", async (req, res) => {
         res.json({
             message: "Captured"
         })
-    } catch (e) {
-        console.log(e);
+    } catch(e) {
+        console.error(e);
         res.status(411).json({
             message: "Error while processing webhook"
         })
     }
+
 })
 
-app.listen(port, () => {
-    console.log("listening on port" + port)
-})
+app.listen(3003);
